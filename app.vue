9f8d5eada185ef9c6e84b983a55e09119c26eb62
Rafael Montero <rmonterodev@gmail.com>
2023-09-15 17:09:30 -0600

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+
+  <div>
+    <h1>Welcome, Rafael's Porfolio</h1>
+    <h2></h2>
+    <p>Software Developer. Site in construction...</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Welcome</h1>
+    <h2>Rafael's Porfolio</h2>
+    
+    <p>Software Developer</p>
+</template>
+
+<script>
+</script>
